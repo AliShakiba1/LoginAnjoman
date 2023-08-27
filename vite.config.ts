41ae12f65e17f4f305eca0b3e4d391b37791331d
AliShakiba <prance5731.mania@gmail.com>
2023-08-27 23:17:06 +0330

    ad

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
